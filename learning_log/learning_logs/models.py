6ca1b406239fdca9d804d70cd68e1193b4fbb828
Belen Telahun <blainetelahun@Bilues-MacBook-Pro.local>
2025-12-01 12:02:58 -0500

    Data Driven Feature

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -7,7 +7,7 @@ class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
 
 
     def __str__(self):
@@ -19,10 +19,16 @@ class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    date_worked = models.DateField(null=True, blank=True)
+    hours_spent = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    exam_score = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "entries"
+        ordering = ['-date_added']
 
     def __str__(self):
         """Return a string representation of the model."""
         return f"{self.text[:50]}..."
+
+
